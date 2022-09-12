97b71c2d09cf69f50c5893444cea8901fa8511aa
Youssef Agagg <YoussefAgagg@gmail.com>
2022-09-12 22:20:20 +0200

    init commit

---
+++ src/main/java/com/github/youssefagagg/automaticirrigationsystem/AutomaticirrigationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssefagagg.automaticirrigationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticirrigationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticirrigationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/youssefagagg/automaticirrigationsystem/AutomaticirrigationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssefagagg.automaticirrigationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticirrigationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
