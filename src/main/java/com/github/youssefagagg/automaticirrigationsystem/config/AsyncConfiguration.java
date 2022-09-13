35bffc559ea5213179a133380756c373b75c62b7
Youssef Agagg <YoussefAgagg@gmail.com>
2022-09-13 12:54:48 +0200

    enable async

---
+++ src/main/java/com/github/youssefagagg/automaticirrigationsystem/config/AsyncConfiguration.java
@@ -0,0 +1,12 @@
+package com.github.youssefagagg.automaticirrigationsystem.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration  {
+
+}
